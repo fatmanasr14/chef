35226e62f0400c1283fea2e68c04c3ed16e64e2f
fatma <117378661+fatmanasr14@users.noreply.github.com>
2024-09-11 18:36:31 +0300

    appRoute and screens

---
+++ lib/core/routes/app_route.dart
@@ -0,0 +1,19 @@
+class Routes{
+  static const String intitlRoute = '/';
+  static const String changeLang = '/changeLang';
+  static const String login = '/login';
+  static const String sendCode = '/sendCode';
+  static const String restPassword = '/restPassword';
+  static const String menuHome = '/menuHome';
+  static const String addMeal = '/addMeal';
+  static const String profileHome = '/profileHome';
+  static const String updateProfile = '/updateProfile';
+  static const String setting = '/setting';
+  static const String changePassword = '/changePassword';
+  static const String home = '/home';
+
+}
+
+class AppRoute{
+
+}
---
+++ lib/features/auth/presentaion/views/splashscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
