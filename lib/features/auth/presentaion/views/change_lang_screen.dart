0c0f7b79ccbe70e89e33c4bd1557c6f1e29a0af7
fatma <117378661+fatmanasr14@users.noreply.github.com>
2024-09-12 15:56:32 +0300

    changelang

--- lib/features/auth/presentaion/views/change_lang_screen.dart
+++ lib/features/auth/presentaion/views/change_lang_screen.dart
@@ -1,10 +1,100 @@
+import 'package:chefapp/core/uitls/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChangeLangScreen extends StatelessWidget {
   const ChangeLangScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset("assets/screen1.png",),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 120.h,
+                    width: 120.w,
+                    child: Image.asset(
+                      "assets/chef_1.png",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  Text(
+                    "Welcome to Chef App",
+                    style: TextStyle(
+                        color: appcolor.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.sp),
+                  ),
+                   SizedBox(
+                    height: 20.h,
+                  ),
+                  Text(
+                    "Please Choose Your Language",
+                    style: TextStyle(
+                        color: appcolor.black,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 13.sp),
+                  ),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    
+                    SizedBox(
+                      width: 140.w,
+                      height: 48.h,
+                      child: ElevatedButton(onPressed: (){}, child: Text("English",style: TextStyle(
+                        fontWeight: FontWeight.w800
+                      ),),
+                      style: ElevatedButton.styleFrom(
+                              backgroundColor:appcolor.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              minimumSize: Size(100, 50), //width
+                              maximumSize: Size(200, double.infinity),
+                            )
+                            ,)
+                            ),
+                      Spacer(),
+                    SizedBox(
+                      width: 140.w,
+                      height: 48.h,
+                      child: ElevatedButton(onPressed: (){},
+                       child: Text("العربيه",style: TextStyle(
+                        fontWeight: FontWeight.w900
+                      ),),
+                      style: ElevatedButton.styleFrom(
+                              backgroundColor:appcolor.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              minimumSize: Size(100, 50), //width
+                              maximumSize: Size(200, double.infinity),
+                            )
+                      )),
+                  ],),
+                  SizedBox(height: 200,)
+
+                ],
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/auth/presentaion/views/splashscreen.dart
+++ lib/features/auth/presentaion/views/splashscreen.dart
@@ -1,10 +1,28 @@
+import 'package:chefapp/core/routes/app_route.dart';
 import 'package:chefapp/core/uitls/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Splashscreen extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
 
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    navigateAfterThreeSecond();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void navigateAfterThreeSecond(){
+    Future.delayed(Duration(seconds: 3)).then((value){
+      Navigator.pushNamed(context, Routes.changeLang);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +34,7 @@ class Splashscreen extends StatelessWidget {
             SizedBox(
               height: 120.h,
               width: 120.w,
-              child: Image.asset("assets/chef_1.png",scale: 3,)),
+              child: Image.asset("assets/chef_1.png",)),
             SizedBox(height: 12.h,),
             Text("Chef App",style: TextStyle(fontSize: 20.sp,fontWeight: FontWeight.bold,color: appcolor.white),)
 
